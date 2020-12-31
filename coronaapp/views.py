b76cea457a89f1cd43cfce665f5a81f65a8181ac
HARSHAL DHARPURE <63531290+harshalD9922@users.noreply.github.com>
2020-12-31 10:21:45 +0530

    Update views.py
    Removing API credentials for saftey.

--- coronaapp/views.py
+++ coronaapp/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 import requests
 import json
 
-url = "https://covid-193.p.rapidapi.com/statistics"
+url = ""
 
 headers = {
-    'x-rapidapi-key': "90f88c6b30msh6878bf751006f7ap146ef6jsnde0275613b83",
-    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    
     }
 
 response = requests.request("GET", url , headers=headers).json()
